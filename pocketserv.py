fe3378e59988635b8ca92b8d33024f2ffb710c61
sergetoure09 <sergetoure09@gmail.com>
2017-11-17 17:54:39 +0000

    some progress

--- pocketserv.py
+++ pocketserv.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from control import *
+import ast
 app=Flask(__name__)
 controller= Controllers()
 option_list = controller.option_cur()
@@ -8,11 +9,6 @@ option_list = controller.option_cur()
 def home():
     return render_template('home.html')
 
-'''@app.route('/convert/')
-def convert():
-    return render_template('home.html',result=to_amount)
-'''
-
 @app.route('/dashboard/')
 def dashboard():   
     return render_template('dashboard.html',option_list=option_list)
@@ -20,6 +16,17 @@ def dashboard():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+@app.route('/convertion', methods=['POST','GET'])
+def convert():
+    if request.method == 'POST':
+        wsfrom_cur=ast.literal_eval(request.form['from_cur'])
+        wsto_cur=ast.literal_eval(request.form['to_cur'])
+        wsfrom_amount=float(request.form['from_amount'])
+        print(type(wsfrom_amount))
+        print(wsfrom_cur,wsto_cur,wsfrom_amount)
+        to_amount = controller.convert_control(wsfrom_cur,wsto_cur,wsfrom_amount)
+        return render_template('dashboard.html',to_amount=to_amount,option_list=option_list)
 
 @app.route('/contact/')
 def contact():
