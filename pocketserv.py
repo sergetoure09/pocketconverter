9ae0cd7b5f229197e422dc2e2826910834ad12c1
sergetoure09 <sergetoure09@gmail.com>
2017-11-17 10:17:27 +0000

    flask ok

--- pocketserv.py
+++ pocketserv.py
@@ -12,6 +12,9 @@ def dashboard():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
